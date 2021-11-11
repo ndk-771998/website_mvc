b4fef08545eb30788d6a14b06d7c2f5b9810b9fc
unknown <nguyenduykhanh827@gmail.com>
2021-11-11 11:14:20 +0700

    Show Product List admin

--- admin/productlist.php
+++ admin/productlist.php
@@ -1,140 +1,59 @@
 ﻿<?php include 'inc/header.php';?>
 <?php include 'inc/sidebar.php';?>
+<?php include '../classes/brand.php';?>
+<?php include '../classes/category.php';?>
+<?php include '../classes/product.php';?>
+<?php include_once '../helpers/format.php';?>
+
 <div class="grid_10">
     <div class="box round first grid">
-        <h2>Post List</h2>
-        <div class="block">  
+        <h2>Product List</h2>
+        <div class="block">
             <table class="data display datatable" id="example">
 			<thead>
 				<tr>
-					<th>Post Title</th>
-					<th>Description</th>
-					<th>Category</th>
+					<th>Id</th>
+					<th>Product Name</th>
+					<th>Price</th>
 					<th>Image</th>
+					<th>Desciption</th>
+					<th>Type</th>
+					<th>Brand</th>
+					<th>Category</th>
 					<th>Action</th>
 				</tr>
 			</thead>
 			<tbody>
-				<tr class="odd gradeX">
-					<td>Trident</td>
-					<td>Internet Explorer 4.0</td>
-					<td>Win 95+</td>
-					<td class="center"> 4</td>
-					<td><a href="">Edit</a> || <a href="">Delete</a></td>
-				</tr>
-				<tr class="even gradeC">
-					<td>Trident</td>
-					<td>Internet Explorer 5.0</td>
-					<td>Win 95+</td>
-					<td class="center">5</td>
-					<td><a href="">Edit</a> || <a href="">Delete</a></td>
-				</tr>
-				<tr class="odd gradeA">
-					<td>Trident</td>
-					<td>Internet Explorer 5.5</td>
-					<td>Win 95+</td>
-					<td class="center">5.5</td>
-					<td><a href="">Edit</a> || <a href="">Delete</a></td>
-				</tr>
-				<tr class="even gradeA">
-					<td>Trident</td>
-					<td>Internet Explorer 6</td>
-					<td>Win 98+</td>
-					<td class="center">6</td>
-					<td><a href="">Edit</a> || <a href="">Delete</a></td>
-				</tr>
-				<tr class="odd gradeA">
-					<td>Trident</td>
-					<td>Internet Explorer 7</td>
-					<td>Win XP SP2+</td>
-					<td class="center">7</td>
-					<td><a href="">Edit</a> || <a href="">Delete</a></td>
-				</tr>
-				<tr class="even gradeA">
-					<td>Trident</td>
-					<td>AOL browser (AOL desktop)</td>
-					<td>Win XP</td>
-					<td class="center">6</td>
-					<td><a href="">Edit</a> || <a href="">Delete</a></td>
-				</tr>
-				<tr class="gradeA">
-					<td>Gecko</td>
-					<td>Firefox 1.0</td>
-					<td>Win 98+ / OSX.2+</td>
-					<td class="center">1.7</td>
-					<td><a href="">Edit</a> || <a href="">Delete</a></td>
-				</tr>
-				<tr class="gradeA">
-					<td>Gecko</td>
-					<td>Firefox 1.5</td>
-					<td>Win 98+ / OSX.2+</td>
-					<td class="center">1.8</td>
-					<td><a href="">Edit</a> || <a href="">Delete</a></td>
-				</tr>
-				<tr class="gradeA">
-					<td>Gecko</td>
-					<td>Firefox 2.0</td>
-					<td>Win 98+ / OSX.2+</td>
-					<td class="center">1.8</td>
-					<td><a href="">Edit</a> || <a href="">Delete</a></td>
-				</tr>
-				<tr class="gradeA">
-					<td>Gecko</td>
-					<td>Firefox 3.0</td>
-					<td>Win 2k+ / OSX.3+</td>
-					<td class="center">1.9</td>
-					<td><a href="">Edit</a> || <a href="">Delete</a></td>
-				</tr>
-				<tr class="gradeA">
-					<td>Gecko</td>
-					<td>Camino 1.0</td>
-					<td>OSX.2+</td>
-					<td class="center">1.8</td>
-					<td><a href="">Edit</a> || <a href="">Delete</a></td>
-				</tr>
-				
-				<tr class="gradeX">
-					<td>Misc</td>
-					<td>Dillo 0.8</td>
-					<td>Embedded devices</td>
-					<td class="center">-</td>
-					<td><a href="">Edit</a> || <a href="">Delete</a></td>
-				</tr>
-				<tr class="gradeX">
-					<td>Misc</td>
-					<td>Links</td>
-					<td>Text only</td>
-					<td class="center">-</td>
-					<td><a href="">Edit</a> || <a href="">Delete</a></td>
-				</tr>
-				<tr class="gradeX">
-					<td>Misc</td>
-					<td>Lynx</td>
-					<td>Text only</td>
-					<td class="center">-</td>
-					<td><a href="">Edit</a> || <a href="">Delete</a></td>
-				</tr>
-				<tr class="gradeC">
-					<td>Misc</td>
-					<td>IE Mobile</td>
-					<td>Windows Mobile 6</td>
-					<td class="center">-</td>
-					<td><a href="">Edit</a> || <a href="">Delete</a></td>
-				</tr>
-				<tr class="gradeC">
-					<td>Misc</td>
-					<td>PSP browser</td>
-					<td>PSP</td>
-					<td class="center">-</td>
-					<td><a href="">Edit</a> || <a href="">Delete</a></td>
-				</tr>
-				<tr class="gradeU">
-					<td>Other browsers</td>
-					<td>All others</td>
-					<td>-</td>
-					<td class="center">-</td>
-					<td><a href="">Edit</a> || <a href="">Delete</a></td>
-				</tr>
+				<?php
+$product = new product();
+$product_list = $product->show_product();
+$fm = new Format();
+if ($product_list) {
+	$i = 0;
+	while ($result = $product_list->fetch_assoc()) {
+		$i++;
+		?>
+	<tr class="odd gradeX">
+		<td><?php echo $i ?></td>
+		<td><?php echo $result['productName'] ?>/td>
+		<td><?php echo $result['price'] ?></td>
+		<td class="center"><img style="width: 160px; height: auto; padding-top: 20px;" src="upload/<?php echo $result['image'] ?>" alt=""></td>
+		<td class="center"><?php echo $fm->textShorten($result['desciption'], 70) ?></td>
+		<td class="center"><?php
+if ($result['type'] == '1') {
+			echo "Feathered";
+		} else {
+			echo "None Feathered";
+		}
+		?></td>
+		<td class="center"><?php echo $result['brandName'] ?></td>
+		<td class="center"><?php echo $result['catName'] ?></td>
+		<td><a href="">Edit</a> || <a href="">Delete</a></td>
+	</tr>
+	<?php
+}
+}
+?>
 			</tbody>
 		</table>
 
--- classes/product.php
+++ classes/product.php
@@ -1,98 +1,103 @@
-<?php 
-	include_once '../lib/database.php';
-	include_once '../helpers/format.php';
- ?>
-<?php 
-	/**
-	 * 
-	 */
-	class product
-	{
-		private $db;
-		private $fm;
+<?php
+include_once '../lib/database.php';
+include_once '../helpers/format.php';
+?>
+<?php
+/**
+ *
+ */
+class product {
+	private $db;
+	private $fm;
 
-		public function __construct()
-		{
-			$this->db = new Database();
-			$this->fm = new Format();
-		}
+	public function __construct() {
+		$this->db = new Database();
+		$this->fm = new Format();
+	}
 
-		public function insert_product($data, $files){
-			$productName = mysqli_real_escape_string($this->db->link, $data['productName']);
-			$brand = mysqli_real_escape_string($this->db->link, $data['brand']);
-			$category = mysqli_real_escape_string($this->db->link, $data['category']);
-			$desciption = mysqli_real_escape_string($this->db->link, $data['desciption']);
-			$type = mysqli_real_escape_string($this->db->link, $data['type']);
-			$price = mysqli_real_escape_string($this->db->link, $data['price']);
+	public function insert_product($data, $files) {
+		$productName = mysqli_real_escape_string($this->db->link, $data['productName']);
+		$brand = mysqli_real_escape_string($this->db->link, $data['brand']);
+		$category = mysqli_real_escape_string($this->db->link, $data['category']);
+		$desciption = mysqli_real_escape_string($this->db->link, $data['desciption']);
+		$type = mysqli_real_escape_string($this->db->link, $data['type']);
+		$price = mysqli_real_escape_string($this->db->link, $data['price']);
 
-			$permited = array('jpg', 'jpng', 'png' , 'gif');
-			$file_name = $_FILES['image']['name'];
-			$file_size = $_FILES['image']['size'];
-			$file_temp = $_FILES['image']['tmp_name'];
+		$permited = array('jpg', 'jpng', 'png', 'gif');
+		$file_name = $_FILES['image']['name'];
+		$file_size = $_FILES['image']['size'];
+		$file_temp = $_FILES['image']['tmp_name'];
 
-			$div = explode('.', $file_name);
-			$file_ext = strtolower(end($div));
-			$unique_image = substr(md5(time()), 0, 10).'.'.$file_ext;
-			$uploaded_image = "upload/".$unique_image;
+		$div = explode('.', $file_name);
+		$file_ext = strtolower(end($div));
+		$unique_image = substr(md5(time()), 0, 10) . '.' . $file_ext;
+		$uploaded_image = "upload/" . $unique_image;
 
-			if ($productName == "" || $brand == "" || $category == "" || $desciption == "" || $type == "" || $price == "" ) {
-				$alert = "Fields must be not empty";
+		if ($productName == "" || $brand == "" || $category == "" || $desciption == "" || $type == "" || $price == "") {
+			$alert = "Fields must be not empty";
+			return $alert;
+		} else {
+			move_uploaded_file($file_temp, $uploaded_image);
+			$query = "INSERT INTO `tbl_product`(`productName`, `catId`, `brandId`, `desciption`, `type`, `price`, `image`) VALUES ('$productName','$category','$brand','$desciption','$type','$price','$unique_image')";
+			$result = $this->db->insert($query);
+			if ($result == true) {
+				$alert = "<span class='success'>Insert Product Successfully<span>";
 				return $alert;
-			}else{
-				move_uploaded_file($file_temp,$uploaded_image);
-				$query = "INSERT INTO `tbl_product`(`productName`, `catId`, `brandId`, `desciption`, `type`, `price`, `image`) VALUES ('$productName','$category','$brand','$desciption','$type','$price','$unique_image')";
-				$result = $this->db->insert($query);
-				if ($result == true) {
-					$alert = "<span class='success'>Insert Product Successfully<span>";
-					return $alert;
-				}else{
-					$alert = "<span class='error'>Insert Product Failed<span>";
-					return $alert;
-				}
+			} else {
+				$alert = "<span class='error'>Insert Product Failed<span>";
+				return $alert;
 			}
 		}
-
-		// public function show_category(){
-		// 	$query = "SELECT * FROM tbl_category ORDER BY catID desc";
-		// 	$result = $this->db->select($query);
-		// 	return $result;
-		// }
-
-		// public function getcatbyId($id){
-		// 	$query = "SELECT * FROM tbl_category WHERE catId = $id";
-		// 	$result = $this->db->select($query);
-		// 	return $result;
-		// }
-
-		// public function update_category($catName, $id){
-		// 	$catName = $this->fm->validation($catName);
-		// 	$catName = mysqli_real_escape_string($this->db->link, $catName);
-		// 	$id = mysqli_real_escape_string($this->db->link, $id);
-		// 	if ($catName == "") {
-		// 		$alert = "Category must be not empty";
-		// 		return $alert;
-		// 	}else{
-		// 		$query = "UPDATE `tbl_category` SET `catName`='$catName' WHERE catId = $id";
-		// 		$result = $this->db->update($query);
-		// 		if ($result == true) {
-		// 			$alert = "<span class='success'>Update Category Successfully<span>";
-		// 			return $alert;
-		// 		}else{
-		// 			$alert = "<span class='error'>Update Category Failed<span>";
-		// 			return $alert;
-		// 		}
-		// 	}
-		// }
-		// public function del_category($id){
-		// 	$query = "DELETE FROM `tbl_category` WHERE catId = $id";
-		// 	$result = $this->db->delete($query);
-		// 	if ($result) {
-		// 		$alert = "<span class='success'>Deleted Category Successfully<span>";
-		// 			return $alert;
-		// 	}else{
-		// 		$alert = "<span class='success'>Deleted Category Failed<span>";
-		// 			return $alert;
-		// 	}
-		// }
 	}
- ?>
+
+	public function show_product() {
+		// $query = "SELECT tbl_product.* , tbl_brand.brandName, tbl_category.catName
+		// FROM tbl_product INNER JOIN tbl_category ON tbl_product.catId = tbl_category.catId
+		// INNER JOIN tbl_brand ON tbl_product.brandId = tbl_brand.brandId
+		// ORDER BY tbl_product.productId desc";
+		$query = "SELECT p.*, c.catName, b.brandName
+		FROM tbl_product as p, tbl_category as c, tbl_brand as b
+		WHERE p.catId = c.catId and p.brandId = b.brandId
+		ORDER BY p.productId desc";
+		$result = $this->db->select($query);
+		return $result;
+	}
+
+	// public function getcatbyId($id){
+	// 	$query = "SELECT * FROM tbl_category WHERE catId = $id";
+	// 	$result = $this->db->select($query);
+	// 	return $result;
+	// }
+
+	// public function update_category($catName, $id){
+	// 	$catName = $this->fm->validation($catName);
+	// 	$catName = mysqli_real_escape_string($this->db->link, $catName);
+	// 	$id = mysqli_real_escape_string($this->db->link, $id);
+	// 	if ($catName == "") {
+	// 		$alert = "Category must be not empty";
+	// 		return $alert;
+	// 	}else{
+	// 		$query = "UPDATE `tbl_category` SET `catName`='$catName' WHERE catId = $id";
+	// 		$result = $this->db->update($query);
+	// 		if ($result == true) {
+	// 			$alert = "<span class='success'>Update Category Successfully<span>";
+	// 			return $alert;
+	// 		}else{
+	// 			$alert = "<span class='error'>Update Category Failed<span>";
+	// 			return $alert;
+	// 		}
+	// 	}
+	// }
+	// public function del_category($id){
+	// 	$query = "DELETE FROM `tbl_category` WHERE catId = $id";
+	// 	$result = $this->db->delete($query);
+	// 	if ($result) {
+	// 		$alert = "<span class='success'>Deleted Category Successfully<span>";
+	// 			return $alert;
+	// 	}else{
+	// 		$alert = "<span class='success'>Deleted Category Failed<span>";
+	// 			return $alert;
+	// 	}
+	// }
+}
+?>
